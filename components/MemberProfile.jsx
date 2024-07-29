a2bfaa313b44ba66a8799994d1978d6d242d6d54
Simon <81940817+simoncheam@users.noreply.github.com>
2024-07-29 15:33:26 -0400

    chore: save progress - add SidebarHeader component with theme toggle

--- components/MemberProfile.jsx
+++ components/MemberProfile.jsx
@@ -1,10 +1,14 @@
 import { UserButton } from '@clerk/nextjs';
+import { currentUser, auth } from '@clerk/nextjs/server';
 
-const MemberProfile = () => {
+const MemberProfile = async () => {
+  const user = await currentUser();
+  // const { userId } = await auth();
   return (
     <div className='px-4 flex items-center gap-2'>
-      <UserButton afterSwitchSessionUrl='/' afterSignOutUrl='/' />
-      MemberProfile
+      <UserButton afterSignOutUrl='/' />
+
+      <p>{user.emailAddresses[0].emailAddress}</p>
     </div>
   );
 };
--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -1,4 +1,27 @@
+'use client';
+
+import { BsMoonFill, BsSunFill } from 'react-icons/bs';
+import { useState } from 'react';
+
+const themes = {
+  winter: 'winter',
+  synthwave: 'synthwave',
+};
+
 const ThemeToggle = () => {
-  return <button className='btn btn-primary btn-sm'>ThemeToggle</button>;
+  const [theme, setTheme] = useState(themes.winter);
+
+  //toggle theme function
+  const toggleTheme = () => {
+    const newTheme = theme === themes.winter ? themes.synthwave : themes.winter;
+    document.documentElement.setAttribute('data-theme', newTheme);
+    setTheme(newTheme);
+  };
+
+  return (
+    <button onClick={toggleTheme} className='btn btn-outline btn-sm'>
+      {theme === themes.winter ? <BsMoonFill /> : <BsSunFill />}
+    </button>
+  );
 };
 export default ThemeToggle;
