8dcd4c6bc780e64e2d6f76d16b56302a99c24944
Simon <81940817+simoncheam@users.noreply.github.com>
2024-07-31 16:40:40 -0400

    chore: save progress - add message handling and submission form to Chat component

--- utils/actions.js
+++ utils/actions.js
@@ -1,5 +1,22 @@
 'use server';
-export const generateChatResponse = async (chatMessage) => {
-  console.log(chatMessage);
-  return 'cool';
+
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const generateChatResponse = async (chatMessages) => {
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [...chatMessages],
+      model: 'gpt-4o-mini',
+      temperature: 0,
+    });
+    console.log(response.choices[0].message);
+    // console.log(response);
+    return response.choices[0].message;
+  } catch (error) {
+    return null;
+  }
 };
