2482e3e7fd5b0d2cce59854e221dcee6bcce069a
Simon <81940817+simoncheam@users.noreply.github.com>
2024-08-10 17:39:12 -0400

    refactor

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
+const isPublicRoute = createRouteMatcher(['/']);
 
-const isProtectedRoute = createRouteMatcher([
-  '/chat(.*)',
-  '/profile(.*)',
-  '/chat(.*)',
-  '/tours(.*)',
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) auth().protect();
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+// const isProtectedRoute = createRouteMatcher(['/chat(.*)', '/profile(.*)', '/chat(.*)', '/tours(.*)']);
